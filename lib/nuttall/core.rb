8dd23fcb662e5f239e48cb4c0850c9a38fb05d4a
Dan Higgins <13562502+lilole@users.noreply.github.com>
2024-11-17 14:54:08 -0600

    Assert for root user and needed system commands

--- lib/nuttall/core.rb
+++ lib/nuttall/core.rb
@@ -9,6 +9,24 @@ module Nuttall
 
     def initialize(config)
       @config = config
+      assert_root_user
+      assert_system_cmds
+    end
+
+    def assert_root_user
+      raise "You must be root to use this tool" if Process.uid != 0
+    end
+
+    def assert_system_cmds
+      required = %w[bash docker lsblk]
+      dirs = ENV["PATH"].split(":")
+      exe = ->(dir, cmd) { File.executable?(File.join(dir, cmd)) }
+
+      missing = required.map do |cmd|
+        dirs.any? { |d| exe[d, cmd] } ? nil : cmd
+      end.compact
+
+      raise "Cannot find needed commands in PATH: #{missing}" if missing.any?
     end
 
     def run
