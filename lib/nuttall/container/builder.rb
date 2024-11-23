4aebe4d57450e7c929a5fe2eb4062886dae7efda
Dan Higgins <13562502+lilole@users.noreply.github.com>
2024-11-22 20:42:54 -0600

    Add missing image files; handle multi images listing; factor image tag value

--- lib/nuttall/container/builder.rb
+++ lib/nuttall/container/builder.rb
@@ -30,18 +30,22 @@ module Container
     def need_new_image?
       run = bash("docker image ls --format=json #{docker_image_name.shellescape}")
       return true if run.fail?
-      info = JSON.parse(run.stdout)
-      if info["Tag"] == Nuttall::VERSION
-        puts "\nWarning: Image #{docker_image_name}:#{Nuttall::VERSION} exists: Overwriting it."
+      run.lines.each do |line|
+        info = JSON.parse(line)
+        if info["Tag"] == docker_image_tag
+          puts "\nWARNING: Image for #{docker_image_tag.inspect} exists: OVERWRITING IT."
+          break
+        end
       end
       true
     end
 
     def user_confirm
-      ask("About to build a new Docker image for version #{Nuttall::VERSION}. Are you sure?", "Yn")
+      ask("About to build a new Docker image for id #{docker_image_tag.inspect}.\n\nAre you sure?", "Yn")
     end
 
     def prep_docker_dir
+      defs_filename = File.basename(config.defaults_file)
       FileUtils.then do |fs|
         fs.rm_rf(docker_dir)
         fs.mkdir_p(docker_dir("app"))
@@ -49,12 +53,15 @@ module Container
         fs.cp_r(app_dir("exe"),                  docker_dir("app"))
         fs.cp_r(app_dir("lib"),                  docker_dir("app"))
         fs.cp_r(app_dir("VERSION"),              docker_dir("app"))
+        fs.cp_r(app_dir("Gemfile"),              docker_dir("app"))
+        fs.cp_r(app_dir("Gemfile.lock"),         docker_dir("app"))
+        fs.cp_r(config.user_file,                docker_dir("app", defs_filename))
       end
     end
 
     def docker_build
       print "\nBuilding..."; $stdout.flush
-      tag = "#{docker_image_name}:#{Nuttall::VERSION}"
+      tag = "#{docker_image_name}:#{docker_image_tag}"
       run = bash("docker buildx build --tag #{tag.shellescape} #{docker_dir.shellescape}")
       if run.ok?
         puts "OK."
@@ -65,6 +72,8 @@ module Container
     end
 
     def docker_image_name = "#{config.file_basename}/server"
+
+    def docker_image_tag = config.config_id
 
     def docker_dir(*subs)
       @docker_dir ||= File.join(Dir.tmpdir, config.file_basename)
