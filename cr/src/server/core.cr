6c4aa9fb7250f72bc9f7225cd6a0be0aa32e3bc7
Dan Higgins <13562502+lilole@users.noreply.github.com>
2025-01-04 09:13:38 -0600

    Factor TLS context to method

--- cr/src/server/core.cr
+++ cr/src/server/core.cr
@@ -11,6 +11,7 @@ module Server
 
     def initialize(config : Hash(Symbol, String))
       @config = config
+      @enc_context = uninitialized OpenSSL::SSL::Context::Server
     end
 
     def run
@@ -19,18 +20,23 @@ module Server
         context.response.print "Hello world at #{Time.local}!"
       end
 
-      enc_context = OpenSSL::SSL::Context::Server.new
-      enc_context.add_options(
-        OpenSSL::SSL::Options::ALL |
-        OpenSSL::SSL::Options::NO_SSL_V2 |
-        OpenSSL::SSL::Options::NO_SSL_V3
-      )
-      enc_context.certificate_chain = "openssl.crt"
-      enc_context.private_key       = "openssl.key"
-
       address = server.bind_tls("0.0.0.0", 8443, enc_context)
       puts "Listening on https://#{address}"
       server.listen
+    end
+
+    def enc_context
+      @enc_context ||= begin
+        OpenSSL::SSL::Context::Server.new.tap do |ctx|
+          ctx.add_options(
+            OpenSSL::SSL::Options::ALL |
+            OpenSSL::SSL::Options::NO_SSL_V2 |
+            OpenSSL::SSL::Options::NO_SSL_V3
+          )
+          ctx.certificate_chain = "openssl.crt"
+          ctx.private_key       = "openssl.key"
+        end
+      end
     end
   end
 end
